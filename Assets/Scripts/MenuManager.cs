ffa4e726745a6289ce37a734b9015b87c56bad0e
thoaidtse <thanhthoai90813@gmail.com>
2025-02-17 14:29:00 +0700

    update high score menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,9 +1,12 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement; // Dùng để chuyển Scene
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject InstructionPanel;
+    public TMP_Text[] scoreTexts;
+    public GameObject HighScorePanel;
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene"); // Chuyển sang Scene có tên "GameScene"
@@ -17,6 +20,22 @@ public class MenuManager : MonoBehaviour
     public void HideInstructions()
     {
         InstructionPanel?.SetActive(false);
+    }
+
+    public void ShowHighScores()
+    {
+        HighScorePanel.SetActive(true);
+
+        for (int i = 0; i < scoreTexts.Length; i++)
+        {
+            int score = PlayerPrefs.GetInt("HighScore" + i, 0);
+            scoreTexts[i].text = (i + 1) + ". " + score;
+        }
+    }
+
+    public void HideHighScores()
+    {
+        HighScorePanel.SetActive(false);
     }
 
     // Hàm thoát game
